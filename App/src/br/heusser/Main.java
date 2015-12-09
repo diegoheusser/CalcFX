044ceeff4225909508517f80acf96b13116f7d43
diego.heusser <diego.heusser@outlook.com>
2015-12-09 12:49:32 -0200

    Initialize project

---
+++ App/src/br/heusser/Main.java
@@ -0,0 +1,33 @@
+package br.heusser;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author Diego Heusser
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("view/screen.fxml"));
+        Scene scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ App/src/br/heusser/controller/Screen.java
@@ -0,0 +1,60 @@
+package br.heusser.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+/**
+ * FXML Controller class
+ *
+ * @author Diego Heusser
+ */
+public class Screen implements Initializable {
+
+    @FXML
+    private Button dot;
+    @FXML
+    private Button zero;
+    @FXML
+    private Button equal;
+    @FXML
+    private Button plus;
+    @FXML
+    private Button one;
+    @FXML
+    private Button two;
+    @FXML
+    private Button three;
+    @FXML
+    private Button minus;
+    @FXML
+    private Button four;
+    @FXML
+    private Button five;
+    @FXML
+    private Button six;
+    @FXML
+    private Button multiplication;
+    @FXML
+    private Button seven;
+    @FXML
+    private Button eight;
+    @FXML
+    private Button nine;
+    @FXML
+    private Button division;
+    @FXML
+    private TextArea textArea;
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
